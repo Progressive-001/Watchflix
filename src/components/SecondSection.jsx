3a469ff7a3d2b4dba1d539e2d62953bc290fb551
Oyerinde Emmanuel <emmanueloyerinde881@gmail.com>
2025-11-12 19:57:24 +0100

    Update On Trending Section

--- src/components/SecondSection.jsx
+++ src/components/SecondSection.jsx
@@ -1,25 +1,16 @@
 import './SecondSection.css'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { MovieContext } from '../context/MovieContext'
-
-import Skeleton from '@mui/joy/Skeleton';
-import MovieSection from './MovieSection';
-import SideBarControl from './SideBarControl';
-
-
-
+import { motion, AnimatePresence } from 'framer-motion'
+import MovieSection from './MovieSection'
+import SideBarControl from './SideBarControl'
 
 export default function SecondSection() {
-   const{isLoading, popular} = useContext(MovieContext);
-
-   // const showVideo = () =>{
-      console.log(popular);
-
-   //    popular.map((item, index)=>{
-   //       console.log(item, index);
-   //    })
-   // }
+   const { popular } = useContext(MovieContext)
+   const [isAnimating, setIsAnimating] = useState(false)
+   const [show, setShow] = useState(false) 
    
+ 
   return (
       <div className='second-section '>
 
@@ -30,16 +21,57 @@ export default function SecondSection() {
          <div className="content-wrapper font-netflix text-headline1 sm:text-headline1 md:text-headline1 lg:text-title2 font-normal">
             <h5 className='font-bold'>Trending Now</h5>
 
-            <div className = 'flex font-netflix p-0 m-[20px] gap-[25px] sm:gap-[20px] md:gap-[30px] lg:gap-[40px]'>
-               
-               {popular.slice(0,4).map((item, index)=>
-                  <div key={index}> 
+            <div className = 'flex font-netflix p-0 my-[20px] gap-[25px] sm:gap-[20px] md:gap-[30px] lg:gap-[36px]'>
+
+               <AnimatePresence>
+                  {show && (
+                     <motion.div
+                        key="sidebar-motion"
+                        initial={{ opacity: 0, x: -100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: -100 }}
+                        transition={{ duration: 0.9, ease: "easeInOut" }}
+                        className='z-[1000] absolute top-0 left-0'
+
+                     >
+                        <SideBarControl 
+                           setShow={setShow} 
+                           isAnimating={isAnimating} 
+                           setIsAnimating={setIsAnimating} 
+                           variant='leftHand'
+                        />
+                     </motion.div>
+                  )}
+               </AnimatePresence>
+
+               {popular.slice(0,10).map((item, index)=>
+                  <div key={index} className={`${isAnimating ? 'transition -translate-x-[1030px] duration-1000': 'translate-x-[0px] duration-1000'}`}> 
                      <MovieSection contents={item} numbers={index}/>
                   </div> 
-               )} 
-            <SideBarControl />
+               )}
+
+               <AnimatePresence>
+                  {!show && (
+                     <motion.div
+                        key="sidebar-motion-right"
+                        initial={{ opacity: 0, x: 100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: 100 }}
+                        transition={{ duration: 0.9, ease: "easeInOut" }}
+                        className='z-[1000] absolute top-0 right-0'
+
+                     >
+                        <SideBarControl 
+                           setShow={setShow} 
+                           isAnimating={isAnimating} 
+                           setIsAnimating={setIsAnimating} 
+                        />
+                     </motion.div>
+                  )}
+               </AnimatePresence>
 
             </div>
+
          </div>
   
       </div>
