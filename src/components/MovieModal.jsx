abb3c8e9faa7345a4e6a25c65bdc2edb5c82c5c7
Oyerinde Emmanuel <155921564+Progressive-001@users.noreply.github.com>
2025-11-26 17:08:16 +0100

    Trending movies (#13)
    * Update On Trending Section
    
    * Update On Trending Section
    
    * Adjusting the container
    
    * Adjusting the container
    
    * New (#10)
    
    * Trending movies (#8)
    
    * Update On Trending Section
    
    * Update On Trending Section
    
    * Adjusting the container
    
    * Adjusting the container
    
    * Trending movies (#9)
    
    * Update On Trending Section
    
    * Update On Trending Section
    
    * Adjusting the container
    
    * Adjusting the container
    
    * Adjusting the container
    
    * updates
    
    * hidden key

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -1,23 +1,52 @@
-// import React from 'react'
-import { useContext } from 'react'
+// eslint-disable-next-line no-unused-vars
+import { motion } from 'framer-motion';
+import { useContext, useEffect } from 'react'
 import { MovieContext } from '../context/MovieContext'
 import './MovieModal.css'
 
+import './MovieModal.css'
 import closeIcon from "/src/assets/landingPage/Cross.svg";
 import SignupArrow from '/src/assets/landingPage/Signup Arrow.svg'
 
 export default function MovieModal({pop, index, isOpen, onClose}) {
-    const { popular} = useContext(MovieContext);
+    const { genreRes} = useContext(MovieContext);
 
-    console.log(popular);
+    useEffect(()=>{
+        if(isOpen){
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflowY = 'visible';
+            document.body.style.overflowX = 'hidden';
+        }
+
+        return ()=>{
+            document.body.style.overflow = 'unset'
+        }
+    }, [isOpen])
+
+    let genreNames = [];
+
+    if(isOpen){
+        console.log(pop.genre_ids);
+        genreRes.map(genre => {
+            if(pop.genre_ids.includes(genre.id)){
+                genreNames.push(genre.name)
+            }
+        })
+    }
 
   return (
-
         <div className='z-[9999]'>
-
-            {isOpen && 
+            {isOpen && (
                 <div onClick={onClose} key={`modal-${index}`} className=' w-full h-full bg-black bg-opacity-50 fixed inset-0 flex items-center justify-center z-[9999] ' >
-                    <div onClick={(e)=> e.stopPropagation()} className='rounded-[8px] mx-[20px] w-full max-w-[650px]  flex flex-col gap-[16px] bg-[#161616] border-[1px] border-gray-600'>
+                    <motion.div 
+                        key="sidebar-motion"
+                        initial={{ opacity: 0, x: 0 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: 0}}
+                        transition={{ duration: 0.7, ease: "easeInOut" }}
+                        onClick={(e)=> e.stopPropagation()} className='rounded-[8px] mx-[20px] w-full max-w-[650px]  flex flex-col gap-[16px] bg-[#161616] border-[1px] border-gray-600'
+                    >
                         <button onClick={onClose} className='closeButton hover:bg-[#161616] absolute rounded-full translate-x-[36rem] translate-y-6 p-2'>
                             <img src={closeIcon} alt="" className=''/>
                         </button>
@@ -28,54 +57,21 @@ export default function MovieModal({pop, index, isOpen, onClose}) {
                             <div className='cover1 w-full h-full'></div>
                         </div>
 
-                        <div className='flex flex-col gap-[40px] items-start font-san font-normal text-msmallbody '>
+                        <div className='flex flex-col gap-[30px] items-start font-san font-normal text-msmallbody '>
                             <h1 className='absolute -translate-y-24 tracking-wide px-[30px] text-blargeTitle font-bebas'>{pop.title}</h1>
-                            {/* <div>
-                                <span>{pop.genre}</span>
-                            </div> */}
-                            <p className='text-justify px-[30px] pt-[30px]'>{pop.overview}</p>
+                            <div className='flex gap-[20px] px-[30px] py-[5px] font-medium'>
+                                <span className='bg-[#414141] rounded-[0.25rem]  py-[0.5rem] px-[0.5rem]'>{new Date(pop.release_date).getFullYear()}</span>
+                                {genreNames.map((name, i) => (<span key={i} className='bg-[#414141] rounded-[0.25rem] py-[0.5rem] px-[0.5rem]'>{name}</span>))}
+                            </div>
+                            <p className='text-justify px-[30px]'>{pop.overview}</p>
                             <button className='buttonContainer-3 flex flex-row justify-center items-center h-[48px] w-full max-w-[156px] font-netflix bg-[#E50914] font-normal text-body'>
                                 Get Started
                                 <img src={SignupArrow} alt="" />
                             </button>
                         </div>
-                    </div>
-                    
-
-                </div>}
-            
+                    </motion.div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
   )
 }
-
-
-// {isOpen && (
-//         <div onClick={onClose} key={`modal-${index}`} className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-[9999]">
-//           <div onClick={(e) => e.stopPropagation()} className="bg-black rounded-lg p-8 max-w-2xl relative">
-//             {/* Close Button */}
-//             <button onClick={onClose} className="absolute top-4 right-4">
-//               <img src={closeIcon} alt="close" className="w-6 h-6" />
-//             </button>
-
-//             {/* Backdrop Image */}
-//             <div className="mb-4">
-//               <img src={`https://image.tmdb.org/t/p/w500${pop.backdrop_path}`} alt={pop.title} className="w-full rounded-lg" />
-//             </div>
-
-//             {/* Content */}
-//             <div>
-//               <h1 className="text-2xl font-bold text-white mb-2">{pop.title}</h1>
-//               <div className="mb-4">
-//                 <span className="text-gray-400">{pop.release_date}</span>
-//               </div>
-//               <p className="text-gray-300 mb-6">{pop.overview}</p>
-//               <button className='buttonContainer-1 flex flex-row justify-center items-center w-full max-w-[858px] font-netflix bg-[#E50914] font-bold text-smallbody'>
-//                 Get Started
-//                 <img src={SignupArrow} alt="" className="ml-2" />
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-    //   )}
